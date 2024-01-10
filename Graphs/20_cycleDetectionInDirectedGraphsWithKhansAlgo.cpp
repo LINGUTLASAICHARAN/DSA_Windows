6eb0aa6afade3798766304c6e14d8e58c83c2125
LINGUTLASAICHARAN <lingutlasaicharanvtp@gmail.com>
2024-01-10 10:03:34 +0530

    cycle detection with khans algo

---
+++ Graphs/20_cycleDetectionInDirectedGraphsWithKhansAlgo.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+// https://bit.ly/3QwPVsi
+
+class Solution
+{
+public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[])
+    {
+
+        vector<int> inDegree(V, 0);
+
+        for (int i = 0; i < V; i++)
+        {
+            for (auto node : adj[i])
+                inDegree[node]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < V; i++)
+        {
+            if (inDegree[i] == 0)
+                q.push(i);
+        }
+        vector<int> ans;
+        while (!q.empty())
+        {
+            int x = q.front();
+            ans.push_back(x);
+            q.pop();
+            for (auto node : adj[x])
+            {
+                inDegree[node]--;
+                if (inDegree[node] == 0)
+                    q.push(node);
+            }
+        }
+        return ans.size() != V;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        cout << obj.isCyclic(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
