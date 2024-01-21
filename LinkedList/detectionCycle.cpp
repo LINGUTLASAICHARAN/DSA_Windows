fc6f42f9e78818918a81f45a072e6e67c491be21
LINGUTLASAICHARAN <lingutlasaicharanvtp@gmail.com>
2024-01-22 00:45:30 +0530

    added comments

--- LinkedList/detectionCycle.cpp
+++ LinkedList/detectionCycle.cpp
@@ -13,20 +13,25 @@ public:
     }
 };
 
+// pass reference of head
 void insertAthead(Node *&head, int x)
 {
+    // create a new node
     Node *n = new Node(x);
+    // make the next of new node = head
     n->next = head;
+    // point head to new node 
     head = n;
 }
 
-void insertAttTail(Node *&head, int x)
+// pass reference of head
+void insertAttTail(Node * &head, int x)
 {
     Node *n = new Node(x);
     Node *temp = head;
     if (head == NULL)
     {
-
+        // there are 0 elements in linked list
         head = n;
         return;
     }
@@ -37,6 +42,7 @@ void insertAttTail(Node *&head, int x)
     temp->next = n;
 }
 
+// dont pass head by reference
 void print(Node *head)
 {
     while (head != NULL)
@@ -46,11 +52,13 @@ void print(Node *head)
     }
     cout << "NULL" << endl;
 }
+
+
 bool detectCycle(Node *head)
 { // floyyds algorithm
     Node *fast = head;
     Node *slow = head;
-    while (!(fast == NULL || fast->next == NULL))
+    while (!(fast == NULL || fast->next == NULL)) // break if fast becomes null or fast->next becomes null
     {
         // when !(x || y) is the condition then ix x or y is true the loop breaks
         fast = fast->next->next;
@@ -62,20 +70,25 @@ bool detectCycle(Node *head)
     }
     return false;
 }
+
 void makeCycle(Node *head, int k)
 {
 
     Node *start;
     int x = 1;
     Node *temp = head;
+    // take temp to end
     while (temp->next != NULL)
     {
+        // store a pointer to start of cycle which is at position k
         if (x == k)
             start = temp;
-        temp = temp->next;
+
+        temp = temp->next; 
         x++;
     }
-    temp->next = start;
+
+    temp->next = start; // make the end point to 
 }
 void removeCycle(Node * &head){ // assuming cycle is there
     Node * slow = head, *fast = head;
@@ -83,11 +96,14 @@ void removeCycle(Node * &head){ // assuming cycle is there
         slow = slow->next;
         fast = fast->next->next;
     }while(fast != slow);
+
+    // now take any pointer to head, fast here
     fast = head;
-    while(fast->next != slow->next){
+    while(fast->next != slow->next){ // move both pointers till they point to the same node
         fast = fast->next;
         slow = slow->next; 
     }
+    // now the pointer that has not been moved is at the end of LL and just point it to null
     slow->next = NULL;
 }
 
